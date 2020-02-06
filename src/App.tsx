f134c9d110f9d9286e0c72048fb56bbea1ddbf71
Michael Landis <michael4css@gmail.com>
2020-02-05 18:21:22 -0800

    Create initial routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, useParams, useRouteMatch } from 'react-router-dom';
+
+const Test = () => {
+  const { recipeId = '' } = useParams();
+  const { url } = useRouteMatch();
+  return (<Switch>
+    <Route path={`${url}/:action`}>
+      <Pathing recipeId={recipeId}/>
+    </Route>
+    <Route>Root: {recipeId}</Route>
+  </Switch>);
+};
+
+const Pathing = ({recipeId}:{recipeId: string}) => {
+  const { action } = useParams();
+  return <>Action: {recipeId} => {action}</>;
+};
 
 const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (<Router>
+    <Switch>
+      <Route path="/new">
+        New
+      </Route>
+      <Route path='/:recipeId'>
+        <Test/>
+      </Route>
+      <Route path='/'>
+        Home
+      </Route>
+    </Switch>
+  </Router>);
 }
 
 export default App;
