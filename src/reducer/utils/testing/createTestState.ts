1d63e269e6fba195d9b68f4f03b006f01628e7e1
Michael Landis <michael4css@gmail.com>
2020-02-29 16:36:57 -0800

    Corrected expected ID behavior

--- src/reducer/utils/testing/createTestState.ts
+++ src/reducer/utils/testing/createTestState.ts
@@ -4,7 +4,7 @@ import createTestRecipe from "./createTestRecipe";
 import createListAndMap from "../createListAndMap";
 
 const createTestState = (ingredients: Ingredient[] = createTestIngredients(), recipes: Recipe[] = [createTestRecipe('foo', false, ingredients)]): ApplicationState => ({
-  id: Math.max(recipes.reduce((max, recipe) => Math.max(max, recipe.id), 0), ingredients.reduce((max, ingredient) => Math.max(max, ingredient.id), 0)),
+  id: Math.max(recipes.reduce((max, recipe) => Math.max(max, recipe.id), 0), ingredients.reduce((max, ingredient) => Math.max(max, ingredient.id), 0)) + 1,
   ingredients: createListAndMap(ingredients),
   recipes: createListAndMap(recipes)
 });
