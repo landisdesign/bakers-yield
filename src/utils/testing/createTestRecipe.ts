01906abdf5e5155f88942832e9cc215c330b7aad
Michael Landis <michael4css@gmail.com>
2020-03-04 15:11:01 -0800

    Update createTestRecipe to include portion updates

--- src/utils/testing/createTestRecipe.ts
+++ src/utils/testing/createTestRecipe.ts
@@ -1,21 +1,33 @@
 import { Ingredient, Recipe } from "../../reducer/state";
 import createTestIngredients from "./createTestIngredients";
 
-const createTestRecipe = (name: string = 'foo', isStarter: boolean = false, ingredients: Ingredient[] = createTestIngredients()): Recipe => ({
+const createTestRecipe = (
+  name: string = 'foo',
+  isStarter: boolean = false,
+  ingredients: Ingredient[] = createTestIngredients(),
+  weightFactor: number = 1
+): Recipe => ({
   name,
   isStarter,
   id: 6,
-  portionSize: 0,
-  portionCount: 0,
-  totalWeight: 0,
-  totalProportion: 0,
+  portionCount: weightFactor,
   measureByPortion: false,
-  ingredients: ingredients.map(ingredient => ({
+  ingredients: ingredients.map((ingredient, index) => ({
     ingredientID: ingredient.id,
-    weight: 0,
-    proportion: 0,
+    weight: (index + 1) * weightFactor,
+    proportion: index + 1,
     percentage: 0
-  }))
+  })),
+  ...ingredients.reduce((props, _, index) => {
+    props.totalWeight += (index + 1) * weightFactor;
+    props.totalProportion += index + 1;
+    props.portionSize += index + 1;
+    return props;
+  }, {
+    totalWeight: 0,
+    totalProportion: 0,
+    portionSize: 0
+  })
 });
 
 export default createTestRecipe;
