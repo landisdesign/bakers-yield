37562381bf2f90f860b5becabd8e3b31c9493f88
Michael Landis <michael4css@gmail.com>
2020-03-11 15:19:52 -0700

    Manage rounding

--- src/Recipe/actions/utils/updateWeights.ts
+++ src/Recipe/actions/utils/updateWeights.ts
@@ -9,16 +9,18 @@ const updateWeights = (recipe: TextRecipe, row: number, weight: string | number)
   }
 
   const proportion = row !== -1 ? textToNumber(recipe.ingredients[row].proportion) : 0;
-  const sanitizedWeight = typeof weight === 'number' ? weight : textToNumber(weight);
+  const weightNumber = typeof weight === 'number' ? weight : textToNumber(weight);
   const weightText = typeof weight === 'number' ? numberToText(weight) : weight;
-  const newTotalWeight = row === -1 ? sanitizedWeight : sanitizedWeight * recipe.totalProportion / proportion;
+  const newTotalWeight = row === -1 ? weightNumber : weightNumber * recipe.totalProportion / proportion;
   recipe.totalWeight = numberToText(newTotalWeight);
   recipe.ingredients.forEach((ingredient, i) => {
-    ingredient.weight = (i === row) ? weightText : numberToText(newTotalWeight * textToNumber(ingredient.proportion) / recipe.totalProportion);
+    ingredient.weight = (i === row) ? weightText : numberToText(roundToThousandths(newTotalWeight * textToNumber(ingredient.proportion) / recipe.totalProportion));
   });
-  recipe.portionCount = recipe.portionSize ? numberToText(newTotalWeight / textToNumber(recipe.portionSize)) : '';
+  recipe.portionCount = recipe.portionSize ? numberToText(roundToThousandths(newTotalWeight / textToNumber(recipe.portionSize))) : '';
 
   return recipe;
 }
 
 export default updateWeights;
+
+const roundToThousandths = (number: number) => Math.round(number * 1000) / 1000;
