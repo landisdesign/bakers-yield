c81f1ff1cf4679ca743bd9f7b9928e90c2988e3a
Michael Landis <michael4css@gmail.com>
2020-03-13 21:04:54 -0700

    Add display filter for starter recipes

--- src/Recipe/IngredientRow.tsx
+++ src/Recipe/IngredientRow.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { DispatcherMap } from 'use-local-slice';
 import { TiTrash } from 'react-icons/ti';
+import { AiFillSchedule } from 'react-icons/ai';
 
 import * as reducers from './actions';
 import { FormState } from '.';
@@ -9,6 +10,8 @@ import Icon from '../elements/Icon';
 import Button from '../elements/Button';
 import TableCell from '../elements/TableCell';
 import TableRow from '../elements/TableRow';
+import { DisplayFilter } from '../elements/AutoCompleteList/getSearchResults';
+import { Ingredient } from '../reducer/state';
 
 interface IngredientProps {
   row: number;
@@ -35,15 +38,20 @@ const IngredientRow: React.FC<IngredientProps> = (props) => {
     : state.ingredients.find(ingredient => ingredient.id === ingredientID)?.name
   ;
 
-  const ingredients = state.ingredients.map(ingredient => `${ingredient.name}${'starterRecipeID' in ingredient ? ' 🥖' : ''}`);
+  const ingredients = state.ingredients;
 
   return <TableRow>
     <TableCell open={edit}><TextInput stretch type='number' disabled={readonly || !edit} value={ingredient.proportion} onChange={e => dispatch.setIngredientProportion({ row, proportion: e.target.value })} /></TableCell>
     <TableCell open={!edit} align='right'>{ingredient.percentage}%</TableCell>
-    <TableCell ><TextInput stretch type='text' disabled={readonly || !edit} value={ingredientText} autoCompleteList={ingredients} onChange={e => dispatch.setIngredient({ row, name: e.target.value })} /></TableCell>
+    <TableCell ><TextInput stretch type='text' disabled={readonly || !edit} value={ingredientText} autoCompleteList={ingredients} displayFilter={ingredientSearchFilter} onChange={e => dispatch.setIngredient({ row, name: e.target.value })} /></TableCell>
     <TableCell open={edit} align='center'><Button disabled={readonly || !edit} onClick={() => dispatch.removeIngredient(row)}><Icon base={TiTrash}/></Button></TableCell>
     <TableCell open={!edit}><TextInput stretch type='number' disabled={readonly || edit} value={ingredient.weight} onChange={e => dispatch.setIngredientWeight({ row, weight: e.target.value })} /></TableCell>
   </TableRow>;
 }
 
 export default IngredientRow;
+
+const ingredientSearchFilter: DisplayFilter<Ingredient> = {
+  search: (ingredient) => ingredient.name,
+  transform: (ingredient, {before, found, after}) => <>{ before }<strong>{ found }</strong>{after}{ 'starterRecipeID' in ingredient ? <> <Icon base={AiFillSchedule} height={1} /></> : ''}</>
+};
--- src/elements/Icon.tsx
+++ src/elements/Icon.tsx
@@ -3,22 +3,32 @@ import styled from 'styled-components';
 
 interface IconProps {
   base: React.ComponentType<any>;
+  height?: number;
 }
 
-const Icon: React.FC<IconProps> = ({base}) => <IconWrapper as={base}/>;
+const Icon: React.FC<IconProps> = ({
+  base,
+  height = 1.5
+}) => <IconWrapper as={base} height={height}/>;
 
 export default Icon;
 
-const color = ({as}: {as: React.ComponentType<any>}) => ({
+const color = ({as}: IconWrapperProps) => ({
   TiTickOutline: '#093',
   TiTimesOutline: '#903',
   TiTrash: '#C30',
-  TiPlusOutline: '#359'
+  TiPlusOutline: '#359',
+  AiFillSchedule: '#963',
 } as {[index: string]: string})[as.displayName ?? ''] ?? '#000';
 
 const IconWrapper = styled.span`
-  height: 1.5em;
+  height: ${ ({height}: IconWrapperProps) => height }em;
   width: auto;
-  vertical-align: -.4em;
+  vertical-align: -${ ({height}: IconWrapperProps) => height * .4 / 1.5 }em;
   color: ${color}
 `;
+
+interface IconWrapperProps {
+  as: React.ComponentType<any>;
+  height: number;
+}
